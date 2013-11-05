e063d81352953f10150ad904db8a8f0d1b3bccdf
Jeff <jleplomet@gmail.com>
2013-11-04 20:21:46 -0800

    stuff for staging env

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/css"
+css_dir = "httpdocs/css"
 sass_dir = "app/assets/sass"
-images_dir = "public/images"
-javascripts_dir = "public/js"
+images_dir = "httpdocs/images"
+javascripts_dir = "httpdocs/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
